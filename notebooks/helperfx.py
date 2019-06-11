432dd19f3d0bba7db76dab7330eee5d925bc0f61
Jaime McCutcheon <jem64@le.ac.uk>
2019-06-11 17:08:36 +0100

    Updates for MOAB

--- notebooks/helperfx.py
+++ notebooks/helperfx.py
@@ -183,3 +183,82 @@ def smooth2(data, window):
     y = np.convolve(data, avg_mask, 'same')
     
     return y
+
+def lickCalc(licks, offset = [], burstThreshold = 0.25, runThreshold = 10, 
+             binsize=60, histDensity = False, adjustforlonglicks='none'):
+    # makes dictionary of data relating to licks and bursts
+    if type(licks) != np.ndarray or type(offset) != np.ndarray:
+        try:
+            licks = np.array(licks)
+            offset = np.array(offset)
+        except:
+            print('Licks and offsets need to be arrays and unable to easily convert.')
+            return
+
+    lickData = {}
+
+    if len(offset) > 0:        
+        lickData['licklength'] = offset - licks[:len(offset)]
+        lickData['longlicks'] = [x for x in lickData['licklength'] if x > 0.3]
+    else:
+        lickData['licklength'] = []
+        lickData['longlicks'] = []
+    
+    if adjustforlonglicks != 'none':
+        if len(lickData['longlicks']) == 0:
+            print('No long licks to adjust for.')
+        else:
+            lickData['median_ll'] = np.median(lickData['licklength'])
+            lickData['licks_adj'] = int(np.sum(lickData['licklength'])/lickData['median_ll'])
+            if adjustforlonglicks == 'interpolate':
+                licks_new = []
+                for l, off in zip(licks, offset):
+                    x = l
+                    while x < off - lickData['median_ll']:
+                        licks_new.append(x)
+                        x = x + lickData['median_ll']
+                licks = licks_new
+        
+    lickData['licks'] = licks
+    lickData['ilis'] = np.diff(np.concatenate([[0], licks]))
+    lickData['shilis'] = [x for x in lickData['ilis'] if x < burstThreshold]
+    lickData['freq'] = 1/np.mean([x for x in lickData['ilis'] if x < burstThreshold])
+    lickData['total'] = len(licks)
+    
+    # Calculates start, end, number of licks and time for each BURST 
+    lickData['bStart'] = [val for i, val in enumerate(lickData['licks']) if (val - lickData['licks'][i-1] > burstThreshold)]  
+    lickData['bInd'] = [i for i, val in enumerate(lickData['licks']) if (val - lickData['licks'][i-1] > burstThreshold)]
+    lickData['bEnd'] = [lickData['licks'][i-1] for i in lickData['bInd'][1:]]
+    lickData['bEnd'].append(lickData['licks'][-1])
+
+    lickData['bLicks'] = np.diff(lickData['bInd'] + [len(lickData['licks'])])    
+    lickData['bTime'] = np.subtract(lickData['bEnd'], lickData['bStart'])
+    lickData['bNum'] = len(lickData['bStart'])
+    if lickData['bNum'] > 0:
+        lickData['bMean'] = np.nanmean(lickData['bLicks'])
+        lickData['bMean-first3'] = np.nanmean(lickData['bLicks'][:3])
+    else:
+        lickData['bMean'] = 0
+        lickData['bMean-first3'] = 0
+    
+    lickData['bILIs'] = [x for x in lickData['ilis'] if x > burstThreshold]
+
+    # Calculates start, end, number of licks and time for each RUN
+    lickData['rStart'] = [val for i, val in enumerate(lickData['licks']) if (val - lickData['licks'][i-1] > runThreshold)]  
+    lickData['rInd'] = [i for i, val in enumerate(lickData['licks']) if (val - lickData['licks'][i-1] > runThreshold)]
+    lickData['rEnd'] = [lickData['licks'][i-1] for i in lickData['rInd'][1:]]
+    lickData['rEnd'].append(lickData['licks'][-1])
+
+    lickData['rLicks'] = np.diff(lickData['rInd'] + [len(lickData['licks'])])    
+    lickData['rTime'] = np.subtract(lickData['rEnd'], lickData['rStart'])
+    lickData['rNum'] = len(lickData['rStart'])
+
+    lickData['rILIs'] = [x for x in lickData['ilis'] if x > runThreshold]
+    try:
+        lickData['hist'] = np.histogram(lickData['licks'][1:], 
+                                    range=(0, 3600), bins=int((3600/binsize)),
+                                          density=histDensity)[0]
+    except TypeError:
+        print('Problem making histograms of lick data')
+        
+    return lickData
